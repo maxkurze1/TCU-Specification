90058cdead174aec3531ac6bbd2973d4c17d3e55
labu <>
2020-05-28 13:21:02 +0200

    test: update HW testcase result addr

--- test/hw/platform.cc
+++ test/hw/platform.cc
@@ -5,10 +5,14 @@
 #define TESTCASE_FAILED      0xAFFEAFFE
 #define TESTCASE_PASSED      0x11111111
 
-#define TESTCASE_RESULT_ADDR 0x00041000
+#if (__riscv_xlen == 32)
+#define TESTCASE_RESULT_ADDR 0x00071000
+#elif (__riscv_xlen == 64)
+#define TESTCASE_RESULT_ADDR 0x10071000
+#endif
 
 volatile uint64_t *ui64_ptr = (uint64_t*)TESTCASE_RESULT_ADDR;
-volatile uint32_t *ui32_ptr = (uintptr_t*)TESTCASE_RESULT_ADDR;
+volatile uint32_t *ui32_ptr = (uint32_t*)TESTCASE_RESULT_ADDR;
 
 void init() {
     ui64_ptr[0] = TESTCASE_FAILED;
