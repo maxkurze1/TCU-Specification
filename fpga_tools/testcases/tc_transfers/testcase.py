22d13fabc042663bed5d49bdd92dbd7ab45b323f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-10-13 09:05:31 +0200

    fpga_tools: improved tc_transfer.
    Use larger data sizes to test with and added arguments to specify the
    FPGA and whether to reset it at the beginning.

--- fpga_tools/testcases/tc_transfers/testcase.py
+++ fpga_tools/testcases/tc_transfers/testcase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import traceback
 import sys
 
@@ -11,20 +12,27 @@ def small_transfers(mem):
     print("Testing small read and write transfers ... ", end="")
 
     # clear everything
-    mem.write_words(0, [0] * 16)
+    mem.write_words(0, [0] * 64)
 
     # write different amounts
-    test_data = [1, 2, 3, 4, 5, 6, 7, 8]
+    test_data = [
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+        0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+    ]
+
     for i in range(1, len(test_data)):
         mem.write_words(0, test_data[0:i])
         words = mem.read_words(0, i)
         assert words == test_data[0:i]
 
     # read and write unaligned
-    mem.write_words(0, [0] * 16)
+    mem.write_words(0, [0] * 64)
     test_bytes = bytes(test_data + test_data)
     for i in range(0, len(test_bytes)):
-        mem.write_bytes(i, test_bytes[i:])
+        # bursts need to be 16-byte aligned atm
+        mem.write_bytes(i, test_bytes[i:], burst=i % 16 == 0)
         b = mem.read_bytes(i, len(test_bytes) - i)
         assert b == test_bytes[i:]
     for i in range(1, len(test_bytes)):
@@ -53,9 +61,14 @@ def large_transfers(mem):
     print("SUCCESS")
 
 def main():
-    # get connection to FPGA, SW12=0000b -> chipid=0
-    fpga_inst = fpga_top.FPGA_TOP(0)
-    # fpga_inst.eth_rf.system_reset()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fpga', type=int)
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+
+    fpga_inst = fpga_top.FPGA_TOP(args.fpga)
+    if args.reset:
+        fpga_inst.eth_rf.system_reset()
 
     mon = NoCmonitor()
 
