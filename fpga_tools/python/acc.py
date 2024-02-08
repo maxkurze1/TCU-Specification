b75bdbc958178b086ce8ac6a8174d151cd1cfc48
Sebastian Haas <sebastian.haas@barkhauseninstitut.org>
2024-02-08 16:42:42 +0100

    fpga_tools: added memory addresses to acc.py

--- fpga_tools/python/acc.py
+++ fpga_tools/python/acc.py
@@ -17,7 +17,19 @@ class AESConfigReg(Enum):
 #todo: keccak config addresses
 
 class ACC():
+    #ASM IMEM: start addr at 0x0, 64kB
+    #ASM DMEM: start addr at 0x10000, 16kB
+    ASM_IMEM_START_ADDR = 0x0
+    ASM_IMEM_SIZE = 0x10000
+    ASM_DMEM_START_ADDR = ASM_IMEM_START_ADDR + ASM_IMEM_SIZE
+    ASM_DMEM_SIZE = 0x4000
+
+    #ACC MEM: start addr at 0x14000, 4kB
+    ACC_MEM_START_ADDR = ASM_DMEM_START_ADDR + ASM_DMEM_SIZE
+    ACC_MEM_SIZE = 0x1000
+
     PICO_INT_COUNT = 32
+
 
     def __init__(self, tcu, mem, pm_num, name:string):
         self.tcu = tcu
@@ -45,14 +57,14 @@ class ACC():
     def acc_getEnable(self):
         return self.mem[self.tcu.config_reg_addr(AccConfigReg.ACC_EN)]
 
-    def pico_getInt(self, int_num):
+    def asm_getInt(self, int_num):
         if (int_num < ACC.PICO_INT_COUNT):
             return (self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_IRQ)] >> int_num) & 0x1
         else:
             print("Interrupt %d not supported for PicoRV32 core. Max = %d" % (int_num, ACC.PICO_INT_COUNT))
             return 0
 
-    def pico_setInt(self, int_num, val:bool):
+    def asm_setInt(self, int_num, val:bool):
         if (int_num < ACC.PICO_INT_COUNT):
             tmp_irq = self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_IRQ)]
             self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_IRQ)] = tmp_irq | (int(val) << int_num)
@@ -61,21 +73,21 @@ class ACC():
 
     #When the interrupt handler is started, the End Of Interrupt (EOI) signals for the handled interrupts go high.
     #The EOI signals go low again when the interrupt handler returns.
-    def pico_getEOI(self, eoi_num):
+    def asm_getEOI(self, eoi_num):
         if (eoi_num < ACC.PICO_INT_COUNT):
             return (self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_EOI)] >> eoi_num) & 0x1
         else:
             print("Interrupt %d not supported for PicoRV32 core. Max = %d" % (eoi_num, ACC.PICO_INT_COUNT))
             return 0
 
-    def pico_getTrap(self):
+    def asm_getTrap(self):
         return self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_TRAP)]
 
     #default stack addr is the end of ASM's DMEM: 0x10000
-    def pico_setStackaddr(self, addr):
+    def asm_setStackaddr(self, addr):
         self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_STACKADDR)] = addr
 
-    def pico_getStackaddr(self):
+    def asm_getStackaddr(self):
         return self.mem[self.tcu.config_reg_addr(AccConfigReg.PICO_STACKADDR)]
 
 
